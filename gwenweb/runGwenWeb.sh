008a2bebbff776133e3691919c9bb365043430fe
bradywood <bradyrwood@gmail.com>
2015-07-05 21:26:50 +1000

    removed rungwenweb from within container.  updated setup to be blank in prep for setup tasks

--- gwenweb/runGwenWeb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/opt/gwen-web/gwen-web-1.0.0-SNAPSHOT/bin/gwen-web /features -b -r /reports -p /opt/gwen-web/gwen.properties --parallel
--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-RUNCMD=docker run -v `pwd`/universal/stage/features/blogs:/features -v `pwd`/reports:/reports --link dockergwenweb_hub_1:hub --rm=true --workdir="/opt/gwen-web" gwenweb /opt/gwen-web/runGwenWeb.sh
-
-SETUP_DOCKER
-
 # setup where the docker-compose.yml file is.  
 # setup an alias or a run command /bin/startGwenWebCompose.sh
 
